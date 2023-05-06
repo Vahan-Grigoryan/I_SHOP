3ed3c2055023fb4d494b4977e083c9d80ae1d3e2
Vahan-Grigoryan <vahan.grigoryan.f@gmail.com>
2023-05-06 10:53:14 +0400

    User Created

--- final_project/fapp/models.py
+++ final_project/fapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    first_name = models.CharField(verbose_name="first name", max_length=150, unique=True)
+    last_name = models.CharField(verbose_name="last name", max_length=150, unique=True)
+    tel = models.IntegerField(blank=True)
+    photo = models.ImageField(upload_to='images/', blank=True)
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=150)
+    post_code = models.IntegerField()
